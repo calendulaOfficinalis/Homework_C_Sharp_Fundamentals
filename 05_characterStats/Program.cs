08e45d2e8f979bf9c8b089a707267469293c3137
calendulaOfficinalis <32248648+calendulaOfficinalis@users.noreply.github.com>
2017-09-30 13:30:48 +0300

    Add files via upload

---
+++ 05_characterStats/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_characterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+
+            Console.Write("Health: |");
+            
+            
+                Console.Write(new string('|',currentHealth ));
+                Console.Write(new string('.',maxHealth-currentHealth));
+            
+            Console.WriteLine("|");
+
+            Console.Write("Energy: |");
+            
+                Console.Write(new string('|', currentEnergy));
+                Console.Write(new string('.', maxEnergy - currentEnergy));
+           
+            Console.WriteLine("|");
+
+        }
+    }
+}
