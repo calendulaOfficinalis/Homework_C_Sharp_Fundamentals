a9dd09498a7c0bd78d41d74865f3ac8c10592d29
calendulaOfficinalis <32248648+calendulaOfficinalis@users.noreply.github.com>
2017-09-30 13:26:51 +0300

    Add files via upload

---
+++ 02_addTwoNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_addTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber;
+
+            Console.WriteLine(firstNumber +" + "+ secondNumber+" = " +sum);
+        }
+
+    }
+}
