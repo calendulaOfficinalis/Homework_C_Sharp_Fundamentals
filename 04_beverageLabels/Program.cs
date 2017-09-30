506c518cb773eb2bed83bce9fc6cac801e9f1132
calendulaOfficinalis <32248648+calendulaOfficinalis@users.noreply.github.com>
2017-09-30 13:29:28 +0300

    Add files via upload

---
+++ 04_beverageLabels/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_beverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string beverageName = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            double energy100 = double.Parse(Console.ReadLine());
+            double sugar100 = double.Parse(Console.ReadLine());
+
+            var energy = volume * energy100 / 100;
+            var sugar = volume * sugar100 / 100;
+
+            Console.WriteLine($"{volume}ml {beverageName}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+
+
+    
+        }
+    }
+}
