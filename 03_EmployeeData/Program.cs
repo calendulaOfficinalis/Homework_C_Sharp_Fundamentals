e878979e96886402b2bdb617b96892770b03658c
calendulaOfficinalis <32248648+calendulaOfficinalis@users.noreply.github.com>
2017-09-30 13:27:53 +0300

    Add files via upload

---
+++ 03_EmployeeData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_EmployeeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double age = double.Parse(Console.ReadLine());
+            double id = double.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {0:D8}",id);
+            Console.WriteLine($"Salary: {0:F2}",salary);
+        }
+
+
+            
+    }
+}
