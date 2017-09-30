00e22495f9e8bac0ce8f01496c3241eb3bd3547e
calendulaOfficinalis <32248648+calendulaOfficinalis@users.noreply.github.com>
2017-09-30 13:24:05 +0300

    Add files via upload

---
+++ 01_DebitCardNum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_DebitCardNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int forthNum = int.Parse(Console.ReadLine());
+
+            Console.Write("{0:D4} {1:D4} {2:D4} {3:D4}",firstNum,secondNum,thirdNum,forthNum);
+
+        }
+    }
+}
